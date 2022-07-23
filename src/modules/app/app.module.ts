3ba49e992bc5f6544f35bd8602866b1247bd1d1e
Blind Kai <blindkai1426@gmail.com>
2022-07-23 14:18:43 +0300

    Module files moved to

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
+import { UsersModule } from '../users/users.module';
+import { PostsModule } from '../posts/posts.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, UsersModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
