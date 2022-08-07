191f47d5e97e30ff154735874078d922fc40facf
Blind Kai <blindkai1426@gmail.com>
2022-08-07 19:34:00 +0300

    Exclude .module.ts, main.ts and database folders from test coverage

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app/app.module';
-import { swaggerConfig } from './modules/documentation/swagger.config';
+import { swaggerConfig } from './documentation/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
